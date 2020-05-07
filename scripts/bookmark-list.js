0f3b6b638cbc6f2e0bb0bca10f95345911bebde3
Charita Nichols <nichols.charita@gmail.com>
2020-05-07 05:37:07 -0400

    changes too bookmark-list.js

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -2,9 +2,24 @@ import $ from 'jquery';
 import store from './store';
 import api from '.api';
 
-const generateBookmarkElement = function (bookmark) {
+const generateMainPage = function () {
+return `
+    <button class="add-bookmark">
+    <span>Add Bookmark</span>
+    </button>
+        <select id="filter-ratings">
+        <span>Filter-Ratings</span>
+        <option value="">--Filter by Rating--</option>
+        <option value="5">5</option>
+        <option value="4">4</option>
+        <option value="3">3</option>
+        <option value="2">2</option>
+        <option value="1">1</option>
+    </select>`;
+};
 
-}
+const generateBookmarkElement = function (bookmark) {}
+
 const generateBookmarkString = function () {
 
 }
@@ -21,7 +36,9 @@ const generateAddBookmark = function () {}
 
 const getBookmarkIdFromElement = function () {}
 
-const handleDelteBookmark = function () {}
+const handleDelteBookmark = function () {
+
+}
 
 const handleFilterBookmark = function () {}
 
@@ -31,4 +48,7 @@ const bindEventListeners = function () {
     
 
 }
-    
+
+//export default {
+    //bindEventListeners,
+    //render
